444a8ad26dd1dfa72b499363eab2ab44f59ca76a
Jeremy Wall <jeremyw@davisinstruments.com>
2021-11-03 15:06:55 -0700

    add phpdoc comments

--- src/weatherlink_v2_api_sdk/signature/SignatureCalculator.php
+++ src/weatherlink_v2_api_sdk/signature/SignatureCalculator.php
@@ -2,11 +2,26 @@
 
 namespace weatherlink_v2_api_sdk\signature;
 
+/**
+ * API Signature calculator utility
+ */
 class SignatureCalculator {
 
+    /**
+     * Default constructor
+     */
     public function __construct() {
     }
 
+    /**
+     * Compute the API Signature for an API call to /stations given the specific parameters
+     *
+     * @param string $apiKey the API Key
+     * @param string $apiSecret the API Secret
+     * @param integer $apiRequestTimestamp the Unix timestamp for when the API request is being made
+     * @param array $stationIds optional integer array of up to 100 station ID numbers
+     * @return string the API Signature
+     */
     public function calculateStationsSignature($apiKey, $apiSecret, $apiRequestTimestamp, $stationIds = array()) {
         $parametersToHash = array(
             "api-key" => $apiKey,
@@ -18,6 +33,15 @@ class SignatureCalculator {
         return $this->calculateSignature($apiSecret, $parametersToHash);
     }
 
+    /**
+     * Compute the API Signature for an API call to /nodes given the specific parameters
+     *
+     * @param string $apiKey the API Key
+     * @param string $apiSecret the API Secret
+     * @param integer $apiRequestTimestamp the Unix timestamp for when the API request is being made
+     * @param array $nodeIds optional integer array of up to 100 node ID numbers
+     * @return string the API Signature
+     */
     public function calculateNodesSignature($apiKey, $apiSecret, $apiRequestTimestamp, $nodeIds = array()) {
         $parametersToHash = array(
             "api-key" => $apiKey,
@@ -29,6 +53,15 @@ class SignatureCalculator {
         return $this->calculateSignature($apiSecret, $parametersToHash);
     }
 
+    /**
+     * Compute the API Signature for an API call to /sensors given the specific parameters
+     *
+     * @param string $apiKey the API Key
+     * @param string $apiSecret the API Secret
+     * @param integer $apiRequestTimestamp the Unix timestamp for when the API request is being made
+     * @param array $sensorIds optional integer array of up to 100 sensor ID numbers
+     * @return string the API Signature
+     */
     public function calculateSensorsSignature($apiKey, $apiSecret, $apiRequestTimestamp, $sensorIds = array()) {
         $parametersToHash = array(
             "api-key" => $apiKey,
@@ -40,6 +73,15 @@ class SignatureCalculator {
         return $this->calculateSignature($apiSecret, $parametersToHash);
     }
 
+    /**
+     * Compute the API Signature for an API call to /sensor-activity given the specific parameters
+     *
+     * @param string $apiKey the API Key
+     * @param string $apiSecret the API Secret
+     * @param integer $apiRequestTimestamp the Unix timestamp for when the API request is being made
+     * @param array $sensorIds optional integer array of up to 100 sensor ID numbers
+     * @return string the API Signature
+     */
     public function calculateSensorActivitySignature($apiKey, $apiSecret, $apiRequestTimestamp, $sensorIds = array()) {
         $parametersToHash = array(
             "api-key" => $apiKey,
@@ -51,6 +93,14 @@ class SignatureCalculator {
         return $this->calculateSignature($apiSecret, $parametersToHash);
     }
 
+    /**
+     * Compute the API Signature for an API call to /sensor-catalog given the specific parameters
+     *
+     * @param string $apiKey the API Key
+     * @param string $apiSecret the API Secret
+     * @param integer $apiRequestTimestamp the Unix timestamp for when the API request is being made
+     * @return string the API Signature
+     */
     public function calculateSensorCatalogSignature($apiKey, $apiSecret, $apiRequestTimestamp) {
         $parametersToHash = array(
             "api-key" => $apiKey,
@@ -59,6 +109,15 @@ class SignatureCalculator {
         return $this->calculateSignature($apiSecret, $parametersToHash);
     }
 
+    /**
+     * Compute the API Signature for an API call to /sensor-catalog given the specific parameters
+     *
+     * @param string $apiKey the API Key
+     * @param string $apiSecret the API Secret
+     * @param integer $apiRequestTimestamp the Unix timestamp for when the API request is being made
+     * @param integer $stationId the station ID
+     * @return string the API Signature
+     */
     public function calculateCurrentSignature($apiKey, $apiSecret, $apiRequestTimestamp, $stationId) {
         $parametersToHash = array(
             "api-key" => $apiKey,
@@ -68,6 +127,17 @@ class SignatureCalculator {
         return $this->calculateSignature($apiSecret, $parametersToHash);
     }
 
+    /**
+     * Compute the API Signature for an API call to /sensor-catalog given the specific parameters
+     *
+     * @param string $apiKey the API Key
+     * @param string $apiSecret the API Secret
+     * @param integer $apiRequestTimestamp the Unix timestamp for when the API request is being made
+     * @param integer $stationId the station ID
+     * @param integer $startTimestamp the Unix timestamp marking the beginning of the time window for the data requested
+     * @param integer $endTimestamp the Unix timestamp marking the end of the time window for the data requested
+     * @return string the API Signature
+     */
     public function calculateHistoricSignature($apiKey, $apiSecret, $apiRequestTimestamp, $stationId, $startTimestamp, $endTimestamp) {
         $parametersToHash = array(
             "api-key" => $apiKey,
